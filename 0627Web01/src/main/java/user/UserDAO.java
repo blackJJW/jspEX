a61d9d75f82a70a8d385104cfc46ea914baa7923
blackJJW <jjinwoo92@gmail.com>
2022-06-27 14:07:05 +0900

    2022=06-27 4

--- 0627Web01/src/main/java/user/UserDAO.java
+++ 0627Web01/src/main/java/user/UserDAO.java
@@ -26,4 +26,29 @@ public class UserDAO {
 			e.printStackTrace();
 		}
 	}
+	
+	// 로그인 기능 수행 함수. 로구인 화면에서 유저가 아이디 비번을 치고 밑의 로그인 버튼을 눌렀을 때 실행
+	public int login(String userID, String userPassword) {
+		String SQL ="SELECT userPassword FROM TABLE_USER WHERE userID=?";
+		// 실행ㅎ할 쿼. 유저가 친 아이티에 해당하는 비밈번호를 가져온다.
+		try {
+			pstmt = conn.prepareStatement(SQL); // 문자열 쿼리를 pstmt에 대입
+			pstmt.setString(1, userID); // 첫번 째 물음표에 userID값 대입
+			// 프로그래밍 언어에서 인덱스는 0부터 시작이지만 쿼리에서는 1부터 시작
+			rs = pstmt.executeQuery();
+			if(rs.next()) { // 결과의 리스트를 받았는데 아음해의 데이터가 있으면
+				if(rs.getString(1).equals(userPassword)){
+					// 남은 것의 첫번재의 값이 login 함수를 호출할 대 전달받은 비밀번호와 같은지 검사
+					return 1; // 로그인 성공
+				} else {
+					return 0; // 로그인 실패
+				}
+				
+			}
+			return -1; // 아이디 없음
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -2; // 데이터 베이스 오류
+	}
 }
